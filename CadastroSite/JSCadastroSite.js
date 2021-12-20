ee71e7b77f6f2e2aafa12ed934d4ecbe0fb204b4
Norrbs <mateusnorberto7@gmail.com>
2021-12-20 17:15:56 -0300

    atualizar olho

--- CadastroSite/JSCadastroSite.js
+++ CadastroSite/JSCadastroSite.js
@@ -14,3 +14,22 @@ function validarSenha() {
 
 // verificar também quando o campo for modificado, para que a mensagem suma quando as senhas forem iguais
 senhaC.addEventListener('input', validarSenha);
+
+var verif = 0
+
+function mostrarSenha() {
+    if (verif == 0) {
+        verif = 1
+        var icon = document.getElementById("iconEye")
+        var label = document.getElementById("passwordLogin")
+        icon.setAttribute("src", "eye-slash.svg")
+        label.setAttribute("type","text")
+    }
+    else{
+        verif = 0
+        var icon = document.getElementById("iconEye")
+        var label = document.getElementById("passwordLogin")
+        icon.setAttribute("src", "eye.svg")
+        label.setAttribute("type","password")
+    }
+}
